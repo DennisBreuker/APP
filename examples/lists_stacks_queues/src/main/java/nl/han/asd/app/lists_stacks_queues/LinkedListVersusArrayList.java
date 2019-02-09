52f1dc2849ee8db236a764848eb47e36d0eead2a
Sander Maijers <Sander.Maijers@han.nl>
2019-02-09 15:50:17 +0100

    Mavenize
    - Make every lesson a Maven module. This loosens coupling, so that problems in one module do not prevent using another.
    - Put ANTLRv4 grammars in default locations, so that generated source code doesn't clutter the source code.
    - Turn `example.cfg` into a Java resource. This makes `nl.han.asd.app.lexing_parsing.Main` insusceptible to IDE 'Run configuration' working directory changes.
    - Remove detailed lesson numbering from package names.

---
+++ examples/lists_stacks_queues/src/main/java/nl/han/asd/app/lists_stacks_queues/LinkedListVersusArrayList.java
@@ -0,0 +1,235 @@
+package nl.han.asd.app.lists_stacks_queues;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author Dennis Breuker
+ * 
+ * Illustration of the use of ArrayList versus LinkedList.
+ * When to use an ArrayList? When to use a LinkedList?
+ * This program gives you the answer!
+ */
+public class LinkedListVersusArrayList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        final int N = 100000;
+        final String TEKST = "test, ja hallo!";
+        
+        long time;
+        List<String> list;
+
+        printLine();
+
+        // insert at start (index 0)
+        // ArrayList
+        list = new ArrayList();
+        time = msInsertAt0(N, list, TEKST);
+        System.out.printf("Insert at start (index 0) ArrayList : %4d ms\n", time);
+        // LinkedList
+        list = new LinkedList();
+        time = msInsertAt0(N, list, TEKST);
+        System.out.printf("Insert at start (index 0) LinkedList: %4d ms\n", time);
+        printLine();
+
+        // insert at index 1
+        // ArrayList
+        list = new ArrayList();
+        time = msInsertAt1(N, list, TEKST);
+        System.out.printf("Insert at index 1 ArrayList : %4d ms\n", time);
+        // LinkedList
+        list = new LinkedList();
+        time = msInsertAt1(N, list, TEKST);
+        System.out.printf("Insert at index 1 LinkedList: %4d ms\n", time);
+        printLine();
+
+        // insert in first tenth (index i/30)
+        // ArrayList
+        list = new ArrayList();
+        time = msInsertAtOneThirtieth(N, list, TEKST);
+        System.out.printf("Insert at one thirtieth (index i/30) ArrayList : %4d ms\n", time);
+        // LinkedList
+        list = new LinkedList();
+        time = msInsertAtOneThirtieth( N, list, TEKST);
+        System.out.printf("Insert at one thirtieth (index i/30) LinkedList: %4d ms\n", time);
+        printLine();
+
+        // insert in first tenth (index 29/30)
+        // ArrayList
+        list = new ArrayList();
+        time = msInsertAtTwentyNineThirtieth(N, list, TEKST);
+        System.out.printf("Insert at one thirtieth (index i*29/30) ArrayList : %4d ms\n", time);
+        // LinkedList
+        list = new LinkedList();
+        time = msInsertAtTwentyNineThirtieth( N, list, TEKST);
+        System.out.printf("Insert at one thirtieth (index i*29/30) LinkedList: %4d ms\n", time);
+        printLine();
+
+        // insert in the middle (index i/2)
+        // ArrayList
+        list = new ArrayList();
+        time = msInsertInTheMiddle(N, list, TEKST);
+        System.out.printf("Insert in the middle (index i/2) ArrayList : %4d ms\n", time);
+        // LinkedList
+        list = new LinkedList();
+        time = msInsertInTheMiddle(N, list, TEKST);
+        System.out.printf("Insert in the middle (index i/2) LinkedList: %4d ms\n", time);
+        printLine();
+
+        // insert at the end-1 (index i-1)
+        // ArrayList
+        list = new ArrayList();
+        time = msInsertAtNearEnd(N, list, TEKST);
+        System.out.printf("Insert at end-1 (index i-1) ArrayList : %4d ms\n", time);
+        // LinkedList
+        list = new LinkedList();
+        time = msInsertAtNearEnd(N, list, TEKST);
+        System.out.printf("Insert at end-1 (index i-1) LinkedList: %4d ms\n", time);
+        printLine();
+
+        // insert at the end
+        // ArrayList
+        list = new ArrayList();
+        time = msInsertAtEnd(N, list, TEKST);
+        System.out.printf("Insert at end (index i) ArrayList : %4d ms\n", time);
+        // LinkedList
+        list = new LinkedList();
+        time = msInsertAtEnd(N, list, TEKST);
+        System.out.printf("Insert at end (index i) LinkedList: %4d ms\n", time);
+        printLine();
+
+        // insert at the end2
+        // ArrayList
+        list = new ArrayList();
+        time = msAdd(N, list, TEKST);
+        System.out.printf("Add to ArrayList : %4d ms\n", time);
+        // LinkedList
+        List list2 = new LinkedList(); // new list, because we are using this in msGetFromMiddle()
+        time = msAdd(N, list2, TEKST);
+        System.out.printf("Add to LinkedList: %4d ms\n", time);
+        printLine();
+
+        // get from the middle (index i/2)
+        // ArrayList
+        time = msGetFromTheMiddle(N, list, TEKST);
+        System.out.printf("Get from the middle (index i/2) ArrayList : %4d ms\n", time);
+        // LinkedList
+        time = msGetFromTheMiddle(N, list2, TEKST);
+        System.out.printf("Get from the middle (index i/2) LinkedList: %4d ms\n", time);
+        printLine();
+    }
+
+    private static long msInsertAt0(final int N, List<String> list, final String TEKST) {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < N; i++) {
+            list.add(0, TEKST);
+        }
+        long stop = System.currentTimeMillis();
+        return stop - start;
+    }
+    
+    private static long msInsertAt1(final int N, List<String> list, final String TEKST) {
+        long start = System.currentTimeMillis();
+        list.add(TEKST); // this one separate
+        for (int i = 1; i < N; i++) {
+            list.add(1, TEKST);
+        }
+        long stop = System.currentTimeMillis();
+        return stop - start;
+    }
+
+//    private static long msInsertAt100(final int N, List<String> list, final String TEKST) {
+//        long start = System.currentTimeMillis();
+//        for (int i=0; i<100; i++) {
+//            list.add(TEKST); // this one separate
+//        }
+//        for (int i = 100; i < N; i++) {
+//            list.add(100, TEKST);
+//        }
+//        long stop = System.currentTimeMillis();
+//        return stop - start;
+//    }
+//
+//    private static long msInsertAt10000(final int N, List<String> list, final String TEKST) {
+//        long start = System.currentTimeMillis();
+//        for (int i=0; i<10000; i++) {
+//            list.add(TEKST); // this one separate
+//        }
+//        for (int i = 10000; i < N; i++) {
+//            list.add(10000, TEKST);
+//        }
+//        long stop = System.currentTimeMillis();
+//        return stop - start;
+//    }
+//
+    private static long msInsertAtOneThirtieth(final int N, List<String> list, final String TEKST) {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < N; i++) {
+            list.add(i/30, TEKST);
+        }
+        long stop = System.currentTimeMillis();
+        return stop - start;
+    }
+
+    private static long msInsertAtTwentyNineThirtieth(final int N, List<String> list, final String TEKST) {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < N; i++) {
+            list.add((int)(i*29.0/30), TEKST);
+        }
+        long stop = System.currentTimeMillis();
+        return stop - start;
+    }
+    private static long msInsertInTheMiddle(final int N, List<String> list, final String TEKST) {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < N; i++) {
+            list.add(i/2, TEKST);
+        }
+        long stop = System.currentTimeMillis();
+        return stop - start;
+    }
+
+    private static long msInsertAtNearEnd(final int N, List<String> list, final String TEKST) {
+        long start = System.currentTimeMillis();
+        for (int i = 1; i < N; i++) {
+            list.add(i-1, TEKST);
+        }
+        long stop = System.currentTimeMillis();
+        return stop - start;
+    }
+    
+    private static long msInsertAtEnd(final int N, List<String> list, final String TEKST) {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < N; i++) {
+            list.add(i, TEKST);
+        }
+        long stop = System.currentTimeMillis();
+        return stop - start;
+    }
+
+    private static long msAdd(final int N, List<String> list, final String TEKST) {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < N; i++) {
+            list.add(TEKST);
+        }
+        long stop = System.currentTimeMillis();
+        return stop - start;
+    }
+
+    private static long msGetFromTheMiddle(final int N, List<String> list, final String TEKST) {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < N; i++) {
+            list.get(i/2);
+        }
+        long stop = System.currentTimeMillis();
+        return stop - start;
+    }
+
+    private static void printLine() {
+        System.out.println("------------------------------------------------------------");
+    }
+}
---
+++ examples/recursie/src/main/java/nl/han/asd/app/recursion/HTree.java
@@ -0,0 +1,49 @@
+package nl.han.asd.app.recursion;
+
+import java.awt.Graphics;
+import javax.swing.JFrame;
+
+public class HTree extends JFrame {
+    public final static int WIDTH = 500;
+    public final static int HEIGHT = 500;
+    public final static double FACTOR = 1.0/Math.sqrt(2);
+
+
+    public HTree() {
+        setSize(WIDTH, HEIGHT);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    private void drawRecursive(Graphics g, int x, int y, int len, boolean hor, int depth) {
+        int x1, y1, x2, y2;
+
+        if (hor) {
+            x1 = x - len;
+            x2 = x + len;
+            y1 = y;
+            y2 = y;
+        } else {
+            y1 = y - len;
+            y2 = y + len;
+            x1 = x;
+            x2 = x;
+        }
+        g.drawLine(x1 + WIDTH / 2, HEIGHT / 2 - y1, x2 + WIDTH / 2, HEIGHT / 2 - y2);
+        if (depth > 0) {
+            int newLen = (int) (len * FACTOR);
+            drawRecursive(g, x1, y1, newLen, !hor, depth - 1);
+            drawRecursive(g, x2, y2, newLen, !hor, depth - 1);
+        }
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        drawRecursive(g, 0, 0, Math.min(WIDTH, HEIGHT)/4, true, 10);
+    }
+
+
+    public static void main(String[] args) {
+        new HTree().setVisible(true);
+    }
+}
