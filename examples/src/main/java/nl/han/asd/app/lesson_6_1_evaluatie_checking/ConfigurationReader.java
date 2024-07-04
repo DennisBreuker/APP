c25af8144da9087370ddf1e791a0f8913c19fd9f
Dennis Breuker <dennis@dennisbreuker.nl>
2024-07-04 14:26:54 +0200

    Added ANTLR4 plugin and folder

--- examples/src/main/java/nl/han/asd/app/lesson_6_1_evaluatie_checking/ConfigurationReader.java
+++ examples/src/main/java/nl/han/asd/app/lesson_6_1_evaluatie_checking/ConfigurationReader.java
@@ -9,6 +9,14 @@ public class ConfigurationReader extends ConfigurationBaseListener {
         properties = new HashMap<String,String>();
     }
 
+//    @Override
+//    public void enterProperty(ConfigurationParser.PropertyContext ctx) {
+//        System.out.println("--- ENTER");
+//        System.out.println(ctx.getChild(0).getText());
+//        System.out.println(ctx.getChild(1).getText());
+//        System.out.println(ctx.getChild(2).getText());
+//    }
+
     @Override
     public void exitProperty(ConfigurationParser.PropertyContext cxt) {
         properties.put(cxt.getChild(0).getText(),
